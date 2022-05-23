3d4c53b19914d9542c91f3067a9de06b245e6c17
54rtor1 <1929sartori@gmail.com>
2022-05-23 14:56:22 -0300

    def answer

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,15 @@ class QuestionsController < ApplicationController
 
   def answer
     @answer = params[:answer]
+    @msg1 = "Great"
+    @msg2 = "Silly question, get dressed and go to work!"
+    @msg3 = "I don't care, get dressed and go to work!"
+    if @answer == "i am going to work right now!"
+      return @msg1
+    elsif @answer.end_with?('?')
+      return @msg2
+    else
+      return @msg3
+    end
   end
 end
